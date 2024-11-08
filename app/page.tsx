04c83242b63b7e31cc106709b0cf4a19fe27908c
OmprakashKadur <kaduromee@gmail.com>
2024-11-08 14:16:28 +0530

    test Deployment 2

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,9 @@ export default function Home() {
     <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div>Welcome to Next Tutorial</div>
-        <div>next App 
-          omee
-        </div>
-        <div>next App</div>
-        <div>next App</div>
+        <div>next App omee</div>
+        <div>next App Tutorial</div>
+        <div className="header">next App</div>
       </main>
     </div>
   );
